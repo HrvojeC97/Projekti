12b0d707f4175ffc8613e6904ad9f8d0bc2e5511
HrvojeC97 <65665206+HrvojeC97@users.noreply.github.com>
2020-05-20 16:29:12 +0200

    Add files via upload

---
+++ Jednostavni projekt.php
@@ -0,0 +1,26 @@
+<?php
+echo'
+<!DOCTYPE html>
+<head>
+
+<link rel="stylesheet" href="css/style.css">
+</head> 
+
+<body> 
+<div class="header">
+  <h1>Header</h1>
+  <p>Zaglavlje</p>
+</div>
+<h1>Naslov!</h1>
+<p>Ovo je text</p> 
+<img src="img/img_lights.jpg" alt="Italian Trulli">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet rhoncus libero, eu pretium eros. Etiam hendrerit elementum quam, sit amet rutrum augue tincidunt non. Vivamus luctus sed ex in rutrum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla finibus tellus vitae porttitor rutrum. Donec ante orci, consequat eget tempor nec, tempor sit amet orci. Donec finibus est leo, vitae vestibulum ante tempus ut. Aenean molestie, massa quis laoreet elementum, justo felis cursus dui, ac maximus ligula dolor sed tortor. Phasellus ante justo, ullamcorper eget euismod quis, blandit at dolor. Morbi ultricies enim et nisi vulputate viverra.</p>
+<p>Duis ultricies metus ante, sit amet accumsan nunc semper id. Praesent condimentum velit non dignissim semper. Morbi consequat libero in bibendum fringilla. Quisque pellentesque, enim sit amet pulvinar efficitur, libero tortor tristique nulla, at pretium lacus tortor at dolor. Sed porta luctus magna non bibendum. Praesent pharetra justo velit, quis suscipit erat feugiat eget. Integer felis tellus, rutrum eget mi eu, fermentum condimentum lorem. Nulla nulla ipsum, ornare at sapien ac, dapibus vestibulum risus. Donec egestas velit ornare magna elementum, elementum lobortis dolor mollis. Ut et mattis tortor. Duis sollicitudin accumsan purus scelerisque malesuada. Quisque interdum mi a dui ultrices cursus. In tellus dui, blandit eu leo sagittis, maximus sodales odio. Maecenas ut ultrices nisl. Ut mauris lacus, lacinia eget dui sed, auctor auctor lacus.</p>
+
+</body> 
+<footer>
+  <p>Author: Hrvoje</p>
+  <p><a href="mailto:hege@example.com">hcardzic@racunarstvo.hr</a></p>
+</footer>
+</html>'
+?>
